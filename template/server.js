a709f0bdd1f7d326c2a3c9c83ff98fd2a0e45038
Manny <manuel_igop@yahoo.com>
2014-01-28 15:48:15 +0800

    added handling of uncaight exception

--- template/server.js
+++ template/server.js
@@ -1,4 +1,20 @@
 #!/bin/env node
 
 var app = require('oils');
+
+//additional server configuration if needed before starting
+
+//var server = app.server; //express server
+
 app.start();
+
+
+
+//so the server won't stop when there's an exception
+//you can remove this if needed.
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
+  if (err) {
+    console.error(err.stack);
+  }
+});
