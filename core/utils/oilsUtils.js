2a7e24961d5765bf07f216d68cc5072fb9f47457
Manny Vergel <manny@mvergel.com>
2017-11-23 02:34:27 +0800

    Added serveBuffer and downloadBuffer in web.utils

--- core/utils/oilsUtils.js
+++ core/utils/oilsUtils.js
@@ -1,5 +1,6 @@
 var extend = require('node.extend');
 var fs = require('fs');
+var getMimeType = require('simple-mime')('application/octet-stream');
 
 module.exports = {
 	extend: extend,
@@ -11,12 +12,31 @@ module.exports = {
 			
 			if (!contentType) {
 				//auto detect mime type
-				var getMimeType = require('simple-mime')('application/octect-stream');
 				contentType = getMimeType(path);
 			}
 
         	res.writeHead(200, {'Content-Type': contentType})
         	res.end(data);
 		})
-	}
+	},
+	serveBuffer: function(buffer, filename, res, contentType) {
+		if (!contentType) {
+			//auto detect mime type
+			contentType = getMimeType(filename);
+		}
+
+    	res.writeHead(200, {'Content-Type': contentType})
+    	res.end(buffer);
+	},
+	downloadBuffer: function(buffer, filename, res, contentType) {
+		if (!contentType) {
+			//auto detect mime type
+			contentType = getMimeType(filename);
+		}
+
+		res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+
+    	res.writeHead(200, {'Content-Type': contentType})
+    	res.end(buffer);
+	},
 }
